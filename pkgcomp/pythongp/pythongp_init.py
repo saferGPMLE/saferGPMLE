df48eb24099df6b2d7953dd56f034792ca065ec4
Julien Bect <julien.bect@centralesupelec.fr>
2021-04-30 09:38:05 +0200

    pythongp_init.py (select_package): Ignore capitalization

--- pkgcomp/pythongp/pythongp_init.py
+++ pkgcomp/pythongp/pythongp_init.py
@@ -1,18 +1,20 @@
 def select_package(pkg):
 
-    if pkg == 'Sklearn':
+    pkg = pkg.lower()
+
+    if (pkg == 'scikit-learn') or (pkg == 'sklearn'):
         from pythongp.wrappers import sklearn_wrapper
         pgp = sklearn_wrapper.sklearn_wrapper()
-    elif pkg == 'GPy':
+    elif pkg == 'gpy':
         from pythongp.wrappers import gpy_wrapper
         pgp = gpy_wrapper.gpy_wrapper()
-    elif pkg == 'GPflow':
+    elif pkg == 'gpflow':
         from pythongp.wrappers import gpflow_wrapper
         pgp = gpflow_wrapper.gpflow_wrapper()
-    elif pkg == 'GPytorch':
+    elif pkg == 'gpytorch':
         from pythongp.wrappers import gpytorch_wrapper
         pgp = gpytorch_wrapper.gpytorch_wrapper()
-    elif pkg == 'ot':
+    elif (pkg == 'openturns') or (pkg == 'ot'):
         from pythongp.wrappers import openturns_wrapper
         pgp = openturns_wrapper.openturns_wrapper()    
     else:
