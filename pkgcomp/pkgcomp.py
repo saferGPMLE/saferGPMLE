3fd4da91553804209e9fd98776f76d519c18ac00
Julien Bect <julien.bect@centralesupelec.fr>
2021-05-09 20:58:39 +0200

    pkgcomp.yml: Concatenate all results

--- pkgcomp/pkgcomp.py
+++ pkgcomp/pkgcomp.py
@@ -1,23 +1,30 @@
+import platform
+import argparse
 import numpy as np
 import pandas as pd
-import argparse
 from pythongp import pythongp_init
-
-all_packages = ['scikit-learn', 'gpy', 'gpytorch', 'gpflow', 'openturns']
 
 # Parse input
 parser = argparse.ArgumentParser()
-parser.add_argument('--pkg', choices=all_packages + ['all'], default='all',
-                    help='Choose one package to be used, or all of them')
+parser.add_argument('--pkg', default='gpy')
 parser.add_argument('--n_train', default='50', type=int)
 parser.add_argument('--filename', default='branin_uniform.csv')
+parser.add_argument('--output', default='')
 args = parser.parse_args()
 
-# List of packages to be tested
-if args.pkg == 'all':
-    package_list = all_packages
+# Package to be tested
+pkg_str = args.pkg
+c = pkg_str.split('==')
+if len(c) == 1:
+    pkg = pkg_str
+    pkg_ver = None
+elif len(c) == 2:
+    pkg = c[0]
+    pkg_ver = c[1]
 else:
-    package_list = [args.pkg]
+    raise Exception("Incorrect package name")
+
+# TODO: check that the correct package version is installed
 
 # Import data
 filename = args.filename
@@ -28,18 +35,27 @@ data = pd.read_csv(filename)
 n_train = args.n_train
 print('n_train =', n_train)
 
+# Output file
+if not args.output:
+    output = None
+else:
+    output = open(args.output, "a")
+
+# OS info
+os_info = platform.system() + '-' + platform.release()
+
 
 ######################################################
 #   A function to compute some metrics               #
 #      for a given package, using a given data set   #
 ######################################################
 
-def compute_metrics(pgp, data, n_train):
+def compute_metrics(pgp, data, n_train, output):
     '''
     Fits GP on multivariate datasets
     Author: S.B
     Description: The following code implements Gaussian Process regression
-    and computes a prediction metric EMRMSE for a python toolbox
+    and computes a prediction metric ERMSPE for a python toolbox
     '''
 
     # Extract & load input data
@@ -69,17 +85,22 @@ def compute_metrics(pgp, data, n_train):
     pgp.init_model(noise=0.0001)
     pgp.optimize(param_opt='MLE', itr=10)
 
-    # Make predictions
-    z_postmean, z_postvar = pgp.predict(x_test)
+    # Extract negative log-likelihood
+    NLL = pgp.get_NLL()
 
-    # Compute EMRMSE
-    emrmse = np.sqrt(np.mean(np.square(
+    # Compute ERMSPE
+    z_postmean, z_postvar = pgp.predict(x_test)
+    ermspe = np.sqrt(np.mean(np.square(
         np.array(z_test) - np.array(z_postmean))))
 
     print("\n")
     print("[compute_metrics]  RESULTS:")
-    print("[compute_metrics]  | NLL    =", pgp.get_NLL())
-    print("[compute_metrics]  | EMRMSE =", emrmse)
+    print("[compute_metrics]  | NLL    =", NLL)
+    print("[compute_metrics]  | EMRMSPE =", ermspe)
+
+    if output:
+        output.write("{:.16e},".format(NLL))
+        output.write("{:.16e}\n".format(ermspe))
 
 
 ####################################################
@@ -88,7 +109,14 @@ def compute_metrics(pgp, data, n_train):
 
 np.random.seed(0)
 
-for pkg in package_list:
-    print("\n\nPython toolbox: ", pkg)
-    pgp = pythongp_init.select_package(pkg)
-    compute_metrics(pgp, data, n_train)
+print("\n\nPython toolbox: ", pkg_str)
+
+if output:
+    output.write(pkg_str + ",")
+    output.write(os_info + ",")
+
+pgp = pythongp_init.select_package(pkg)
+compute_metrics(pgp, data, n_train, output)
+
+if output:
+    output.close()
