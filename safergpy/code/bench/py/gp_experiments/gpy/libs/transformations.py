cb690de9e6d790b3877a08cd0fa57679c27d8411
Julien Bect <julien.bect@centralesupelec.fr>
2021-06-27 14:22:37 +0200

    maint: Fix flake8 E301: Expected 1 blank line, found 0
    https://www.flake8rules.com/rules/E301.html

--- safergpy/code/bench/py/gp_experiments/gpy/libs/transformations.py
+++ safergpy/code/bench/py/gp_experiments/gpy/libs/transformations.py
@@ -18,31 +18,41 @@ logger = logging.getLogger(__name__)
 
 class IdentityPosReals(Transformation):
     domain = domains._POSITIVE
+
     def f(self, x):
         return x
+
     def finv(self, x):
         return x
+
     def initialize(self, f):
         if np.any(f < 0.):
             logger.info("Warning: changing parameters to satisfy constraints")
         return np.abs(f)
+
     def gradfactor(self, f, df):
         return np.einsum('i,i->i', df, np.exp(f-f))
+
     def __str__(self):
         return '+ve'
 
 
 class ReciprocalSquareRoot(Transformation):
     domain = domains._POSITIVE
+
     def f(self, x):
         return 1/np.sqrt(x)
+
     def finv(self, x):
         return 1/(x ** 2)
+
     def initialize(self, f):
         if np.any(f < 0.):
             logger.info("Warning: changing parameters to satisfy constraints")
         return np.abs(f)
+
     def gradfactor(self, f, df):
         return np.einsum('i,i->i', df, (-0.5) * (f ** (-1.5)))
+
     def __str__(self):
         return '+sq'
